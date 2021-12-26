8df20d57bd3f3483a8934d69b02fe425c7b12137
loco-co <loco2925@gmail.com>
2021-12-26 20:49:13 +0900

    Background Image

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const imgs = [
+    "03712424832578407730.png",
+    "25337689005706279322.png",
+    "31521796070669573658.png",
+    "33621171160959027994.png",
+]
+
+const todayImage = imgs[Math.floor(Math.random() * imgs.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${todayImage}`;
+bgImage.classList.add("bgImage");
+
+document.body.appendChild(bgImage);
