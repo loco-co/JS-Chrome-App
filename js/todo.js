411ca76dcd21cf403975f3ae3df0b556b9328985
loco-co <loco2925@gmail.com>
2021-12-27 22:43:18 +0900

    Load To Do

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+let toDoArray = []; 
+const TODO_KEY = "todo-list";
+
+function saveToDo(newToDo) {
+    toDoArray.push(newToDo);
+    localStorage.setItem(TODO_KEY, JSON.stringify(toDoArray));
+}
+
 function removeToDo(event) {
     const toDo = event.target.parentElement;
     toDo.remove();
@@ -25,6 +33,14 @@ function handleToDoSubmit(event) {
     toDoInput.value = "";
     console.log(newToDo);
     paintToDo(newToDo);
+    saveToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const storedToDo = localStorage.getItem(TODO_KEY);
+
+if(storedToDo !== null) {
+    toDoArray = JSON.parse(storedToDo);
+    toDoArray.forEach(paintToDo);
+}
